60344c19b164c33e4c3b49fa50fbcfc285512942
zhulinfeng <zhulinfeng@yunjingit.com>
2019-02-22 12:02:19 +0800

    增加对 protobuf api 的性能测试

--- hardsm/src/main/wst/test/smtool/test_cryp.c
+++ hardsm/src/main/wst/test/smtool/test_cryp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/time.h>
 #include <assert.h>
 #include "../../proto/sm.pb-c.h"
 #include "../../include/sm_api.h"
@@ -13,6 +14,7 @@ static char *origin_data = "0123456701234567012345670123456701234567012345670123
 static char *encrypt_result_anylen = "eefb0602800038b355744473abe2a292eefb0602800038b355744473abe2a292eefb0602800038b355744473abe2a2927d2a6bf91f625fa4db48fa19cb8645c8";
 static char *encrypt_result = "eefb0602800038b355744473abe2a292eefb0602800038b355744473abe2a292eefb0602800038b355744473abe2a292eefb0602800038b355744473abe2a2921bda5859da7534a80121a1e79b859431";
 
+static long current_timestamp();
 static void test_digest();
 static void test_digest_section();
 static void test_random();
@@ -27,6 +29,12 @@ static void test_decrypt_section();
 static void test_encrypt_section_modlen();
 static void test_decrypt_section_modlen();
 static void test_sign_verify();
+static void digest_alot();
+static void random_alot();
+static void encrypt_alot();
+static void decrypt_alot();
+static void sign_alot();
+static void verify_alot();
 
 
 void test_crypto() {
@@ -44,6 +52,13 @@ void test_crypto() {
     test_encrypt_section_modlen();
     test_decrypt_section_modlen();
     test_sign_verify();
+
+    digest_alot();
+    random_alot();
+    encrypt_alot();
+    decrypt_alot();
+    sign_alot();
+    verify_alot();
 }
 
 static void test_digest() {
@@ -336,4 +351,188 @@ static void test_sign_verify() {
     IntValue *int_value = (IntValue *)response->int_value;
     printf("verify result: %d\n", int_value->value);
     response__free_unpacked(response, NULL);
+}
+
+static void digest_alot() {
+    uint8_t out[1024 * 32]  ={0};
+    int errors = 0;
+    int counts = 10000;
+
+    long start_timestamp = current_timestamp();
+
+    int i;
+    for (i = 0; i < counts; i++) {
+        char *data = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc";
+        int l = api_digest(0, 0, data, strlen(data), out);
+        Response *response = response__unpack(NULL, l, out);
+        if (response->code != 0) errors++;
+        response__free_unpacked(response, NULL);
+    }
+
+    long stop_timestamp = current_timestamp();
+
+    printf("digest performance test result: \n");
+    printf("errors: %d\n", errors);
+    printf("counts: %d\n", counts);
+    printf("time: %ld\n", stop_timestamp - start_timestamp);
+}
+
+static void random_alot() {
+    uint8_t out[1024 * 32]  ={0};
+    int errors = 0;
+    int counts = 10000;
+
+    long start_timestamp = current_timestamp();
+
+    int i;
+    for (i = 0; i < 10000; i++) {
+        int l = api_random(0, 0, 16, out);
+        Response *response = response__unpack(NULL, l, out);
+        if (response->code != 0) errors++;
+        response__free_unpacked(response, NULL);
+    }
+
+    long stop_timestamp = current_timestamp();
+
+    printf("random performance test result: \n");
+    printf("errors: %d\n", errors);
+    printf("counts: %d\n", counts);
+    printf("time: %ld\n", stop_timestamp - start_timestamp);
+}
+
+static void encrypt_alot() {
+    uint8_t out[1024 * 32]  ={0};
+    int errors = 0;
+    int counts = 10000;
+
+    long start_timestamp = current_timestamp();
+
+    int i;
+    for (i = 0; i < 10000; i++) {
+        int l = api_encrypt(0, 0, hex_secret, NULL, origin_data_anylen, strlen(origin_data_anylen), out);
+        Response *response = response__unpack(NULL, l, out);
+        if (response->code != 0) errors++;
+        response__free_unpacked(response, NULL);
+    }
+
+    long stop_timestamp = current_timestamp();
+
+    printf("encrypt performance test result: \n");
+    printf("errors: %d\n", errors);
+    printf("counts: %d\n", counts);
+    printf("time: %ld\n", stop_timestamp - start_timestamp);
+}
+
+static void decrypt_alot() {
+    uint8_t out[1024 * 32]  ={0};
+    int errors = 0;
+    int counts = 10000;
+
+    long start_timestamp = current_timestamp();
+
+    int i;
+    for (i = 0; i < 10000; i++) {
+        char data[1024] = {0};
+        int data_len = sizeof(out);
+        from_hex(data, &data_len, encrypt_result_anylen);
+
+        int l = api_decrypt(0, 0, hex_secret, NULL, data, data_len, out);
+        Response *response = response__unpack(NULL, l, out);
+        if (response->code != 0) errors++;
+        response__free_unpacked(response, NULL);
+    }
+
+    long stop_timestamp = current_timestamp();
+
+    printf("decrypt performance test result: \n");
+    printf("errors: %d\n", errors);
+    printf("counts: %d\n", counts);
+    printf("time: %ld\n", stop_timestamp - start_timestamp);
+}
+
+static void sign_alot() {
+    uint8_t out[1024 * 32]  ={0};
+    int errors = 0;
+    int counts = 10000;
+
+    char private_key[256] = {0};
+    char public_key[256] = {0};
+
+    // generate key pair
+    int l = api_generate_keypair(0, 0, out);
+    Response *response = response__unpack(NULL, l, out);
+    check_response(response);
+    KeyPair *key_pair = (KeyPair *)response->key_pair;
+    strncpy(private_key, key_pair->private_key, sizeof(private_key));
+    strncpy(public_key, key_pair->public_key, sizeof(public_key));
+    response__free_unpacked(response, NULL);
+
+    long start_timestamp = current_timestamp();
+
+    int i;
+    for (i = 0; i < 10000; i++) {
+        l = api_sign(0, 0, private_key, origin_data, out);
+        response = response__unpack(NULL, l, out);
+        if (response->code != 0) errors++;
+        response__free_unpacked(response, NULL);
+    }
+
+    long stop_timestamp = current_timestamp();
+
+    printf("sign performance test result: \n");
+    printf("errors: %d\n", errors);
+    printf("counts: %d\n", counts);
+    printf("time: %ld\n", stop_timestamp - start_timestamp);
+}
+
+static void verify_alot() {
+    uint8_t out[1024 * 32]  ={0};
+    int errors = 0;
+    int counts = 10000;
+
+    char private_key[256] = {0};
+    char public_key[256] = {0};
+    char signature[256] = {0};
+
+    // generate key pair
+    int l = api_generate_keypair(0, 0, out);
+    Response *response = response__unpack(NULL, l, out);
+    check_response(response);
+    KeyPair *key_pair = (KeyPair *)response->key_pair;
+    strncpy(private_key, key_pair->private_key, sizeof(private_key));
+    strncpy(public_key, key_pair->public_key, sizeof(public_key));
+    response__free_unpacked(response, NULL);
+
+    // sign
+    l = api_sign(0, 0, private_key, origin_data, out);
+    response = response__unpack(NULL, l, out);
+    check_response(response);
+    StrValue *str_value = (StrValue *)response->str_value;
+    strncpy(signature, str_value->value, sizeof(signature));
+    response__free_unpacked(response, NULL);
+
+
+    long start_timestamp = current_timestamp();
+
+    int i;
+    for (i = 0; i < 10000; i++) {
+        l = api_verify(0, 0, public_key, origin_data, signature, out);
+        response = response__unpack(NULL, l, out);
+        if (response->code != 0) errors++;
+        response__free_unpacked(response, NULL);
+    }
+
+    long stop_timestamp = current_timestamp();
+
+    printf("verify performance test result: \n");
+    printf("errors: %d\n", errors);
+    printf("counts: %d\n", counts);
+    printf("time: %ld\n", stop_timestamp - start_timestamp);
+}
+
+static long current_timestamp() {
+    struct timeval te;
+    gettimeofday(&te, NULL); // get current time
+    long milliseconds = te.tv_sec * 1000L + te.tv_usec / 1000; // calculate milliseconds
+    return milliseconds;
 }
